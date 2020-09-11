74806512957824ab41537c5039c70b3089a6c90b
bitcoinmonster23 <39277307+bitcoinmonster23@users.noreply.github.com>
2020-09-11 16:19:49 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, Filters, InlineKeyboardMarkup, InlineKeyboardButton
+import custom_utils
+import os
+from decouple import config
+
+
+app2 = Client(
+    'filmy_bot',
+    api_id = '1319289' ,
+    api_hash = '8f5127ed38135aa349a7384f547a309c' ,
+    bot_token = '1263380136:AAG_kErZYKbrBNxpZzxcOlTo32ffLHQOaJQ' ,
+)
+
+
+@app2.on_message(Filters.regex('http'))
+def post(client, message):
+    if message.chat.username == 'screlizer':
+        url = message.text
+        text = custom_utils.parsing(url)
+        client.send_message(
+            'filmy',
+            text[0],
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton('🎥 Смотреть онлайн!', url=text[1]),
+                InlineKeyboardButton('🔎 Поиск фильмов!', url='http://w.ikino.site/index.php?do=search')
+            ]]),
+        )
+
+
+app2.run()
+
